a3f81e0bff7a96ade2ef72cfe3b3971dd2c94119
nickkking <42115833+nickkking@users.noreply.github.com>
2021-05-30 16:31:40 +0800

    Update docx2txt.py

--- v3批量文件中截取序列/docx2txt.py
+++ v3批量文件中截取序列/docx2txt.py
@@ -12,7 +12,7 @@ def docx2txt(path,path1):
             txt_path = os.path.join(path1, str(filename.strip('.docx'))+'.txt')#terminal path
             if not os.path.isdir(path1):  # 如果 _path 目录不存在，则创建
                 os.makedirs(path1)
-            f = open(txt_path, 'w', encoding='utf-8')
+            f = open(txt_path, 'w', encoding='utf-8')#utf-8！！！！！！！！！！！！！！
 
 
             for paragraph in tqdm(document.paragraphs):#进度条
